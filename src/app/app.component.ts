7f11176883491edddab99e8b4555b7d32c7eb65a
Antoine Perin <perin.antoine@sfr.fr>
2022-04-23 20:43:34 +0200

    deuxieme commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,36 +6,35 @@ import { FaceSnap } from './models/face-snap.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
 
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
-
-  ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      0
-    );
-
-    this.myOtherSnap=new FaceSnap(
-      'Antoine',
-      'Le plus bg',
-      'assets/img/photo.jpg',
-      new Date(),
-      2
-    )
-
-    this.myLastSnap= new FaceSnap(
-      'Goudurix',
-      'La meilleure attraction du parc',
-      'https://static.wixstatic.com/media/84686c_96b15b58df61431aa41c71cfbb4789cf~mv2.jpg/v1/fill/w_740,h_555,al_c,q_90/84686c_96b15b58df61431aa41c71cfbb4789cf~mv2.webp',
-      new Date(),
-      3
-    )
+  ngOnInit() {
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 160,
+        location: 'Paris'
+      },
+      {
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 40,
+        location: 'la montagne'
+      },
+      {
+        title: 'Un bon repas',
+        description: 'Mmmh que c\'est bon !',
+        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 0
+      }
+    ];
   }
-  
 }
